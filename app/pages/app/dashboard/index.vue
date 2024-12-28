186525304b7dff471773b601e750a935266eff43
Ayax0 <simon.gander@vistectic.com>
2024-12-28 16:33:34 +0100

    init

---
+++ app/pages/app/dashboard/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const count = ref<number>(0);
+
+function increment() {
+  count.value++;
+}
+</script>
+
+<template>
+  <div>
+    <h1>Test</h1>
+    <div>Count: {{ count }}</div>
+
+    <div class="w-20 border">Test</div>
+
+    <Button @click="increment">Increment</Button>
+    <TestComponent />
+    <Input />
+  </div>
+</template>
